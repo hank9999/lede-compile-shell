d767554d19a62eb5cae8227f8112271d9e3a37f1
hank9999 <hank9999@qq.com>
2020-02-22 16:37:17 +0800

    fix bug

--- LEDE-Compile-Preparation.sh
+++ LEDE-Compile-Preparation.sh
@@ -13,7 +13,7 @@ sudo apt update
 echo "-----------------------"
 echo "1.2. Install Softs"
 echo "-----------------------"
-sudo apt -y install python3
+sudo apt -y install python3.5
 sudo apt -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler
 echo "-----------------------"
 echo "1.3. Clone Source code"
