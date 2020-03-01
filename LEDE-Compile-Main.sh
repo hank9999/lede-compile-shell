0bedc430948688bfc6248d332ea488e58600d8c1
hank9999 <hank9999@qq.com>
2020-03-01 22:25:27 +0800

    fix install

--- LEDE-Compile-Main.sh
+++ LEDE-Compile-Main.sh
@@ -13,7 +13,19 @@ sudo apt update
 echo "-----------------------"
 echo "1.2. Install Softs"
 echo "-----------------------"
+# From https://build-scripts.project-openwrt.eu.org/init_build_environment.sh
+sudo apt full-upgrade -y
 sudo apt install -y build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core p7zip p7zip-full msmtp libssl-dev texinfo libreadline-dev libglib2.0-dev xmlto qemu-utils libelf-dev autoconf automake libtool autopoint ccache curl wget vim nano python python3 python-pip python3-pip python-ply python3-ply haveged lrzsz device-tree-compiler scons squashfs-tools
+sudo apt install -y gcc-8 g++-8 gcc-8-multilib g++-8-multilib
+sudo ln -sf "/usr/bin/gcc-8" "/usr/bin/gcc"
+sudo ln -sf "/usr/bin/g++-8" "/usr/bin/g++"
+sudo ln -sf "/usr/bin/gcc-ar-8" "/usr/bin/gcc-ar"
+sudo ln -sf "/usr/bin/gcc-nm-8" "/usr/bin/gcc-nm"
+sudo ln -sf "/usr/bin/gcc-ranlib-8" "/usr/bin/gcc-ranlib"
+sudo apt install -y nodejs yarn
+npm config set registry "https://registry.npm.taobao.org/" --global
+yarn config set registry "https://registry.npm.taobao.org/" --global
+# sudo apt install -y build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core p7zip p7zip-full msmtp libssl-dev texinfo libreadline-dev libglib2.0-dev xmlto qemu-utils libelf-dev autoconf automake libtool autopoint ccache curl wget vim nano python python3 python-pip python3-pip python-ply python3-ply haveged lrzsz device-tree-compiler scons squashfs-tools
 # sudo bash <(curl -s https://build-scripts.project-openwrt.eu.org/init_build_environment.sh)
 if [ `whoami` = "root" ];then
 useradd -m -s /bin/bash lede-compile
